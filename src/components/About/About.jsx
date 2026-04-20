16cb20c177946ba5a65e198f23cf342334c6ca25
azeem251 <azeemabdul13696@gmail.com>
2026-04-20 16:09:39 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,22 +17,22 @@ const About = () => {
                         <div className='row g-5'>
                             <div className='col-lg-7 wow animate__animated animate__slow 3s  animate__lightSpeedInLeft'>
                                 <div className='about__content_box'>
-                                    <h3>Frontend Developer</h3>
+                                    <h3>Flutter Developer</h3>
 
-                                    <p>I am Skilled Frontend Developer & Web Developer  With Over 1 Year Of Experience In The Industry.My Passion Lies In Creating Captivating UI Designs  and App Design And Implementing Them Through Flutter Developer.Take Pride In Staying Up-To-Date With Current Design Trends And Leveraging My Creativity To Produce Visually Application And User-Friendly Websites.Through My Career,I Have Developed A Deep Understanding Of User Experince(UX) And User Interface (UI) Principles.</p>
+                                    <p>I am Skilled Flutter Developer & Android Developer With Over 3 Years Of Experience In The Industry.My Passion Lies In Creating Captivating UI Designs and App Design And Implementing Them Through Flutter Developer.Take Pride In Staying Up-To-Date With Current Design Trends And Leveraging My Creativity To Produce Visually Application And User-Friendly Websites.Through My Career,I Have Developed A Deep Understanding Of User Experince(UX) And User Interface (UI) Principles.</p>
                                     <div className='about_linkls'>
                                         <div className='aboutlinks-flex'>
                                             <ul>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Website :-</strong> www.azeemansari.com</Link></li>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='tel:8791328156'><strong>Phone :-</strong>+91 8791328156 </Link></li>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='mailto:azeemabdul13696@gmail.com'><strong>Email :-</strong>azeemabdul13696@gmail.com</Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Website :-</strong>jyoti-portfolio-rho.vercel.app</Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='tel:8791328156'><strong>Phone :-</strong>+91 7617867769 </Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='mailto:jyotiprajapati4852@gmail.com'><strong>Email :-</strong>jyotiprajapati4852@gmail.com</Link></li>
                                             </ul>
                                         </div>
                                         <div className='aboutlinks-flex'>
                                             <ul>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Degree :-</strong>B.C.A (CS)</Link></li>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Date-Of-Barth :- </strong> 13 DEC 2000</Link></li>
-                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>City :-</strong> Greater Noida(U.P) India..</Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Degree :-</strong>B.Tech (Information Technology)</Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>Date-Of-Barth :- </strong> 02 AUG 1997</Link></li>
+                                                <li><i className='fa-solid fa-arrow-right-to-bracket' id='arrow-id'></i><Link to='/'><strong>City :-</strong> Gadhwa Chowk Ward No. 4 pipraich Gorakhpur.</Link></li>
                                             </ul>
                                         </div>
                                     </div>
